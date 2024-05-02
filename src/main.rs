36c81bd52aa88b2842174d6ed3b3a3ea10b531b0
osianrjones <osianrj03@gmail.com>
2024-05-02 18:56:46 +0100

    Git command 'init'

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
 #[allow(unused_imports)]
 use std::env;
+use std::fmt::format;
 #[allow(unused_imports)]
 use std::fs;
+use std::vec;
 
 fn main() {
 
     // Create default .git directory
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+    match args[1].as_str() {
+        "init" => {
+            let init_result = init();
+            if init_result.is_err() {
+                println!("Unable to create git directory.")
+            }
+        }
+        _ => println!("Unknown command: {}", args[1])
     }
+}
+
+/*
+    A function used to upon a "git init" call.
+    Initializes all default directory required by git.
+*/
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn init() -> Result<(), std::io::Error>{
+    //Create .git exists
+    let git_root_creation = fs::create_dir(".git")?;
+
+    let init_dirs = vec![".git/objects", ".git/refs", ".git/HEAD"];
+    for dirs in init_dirs {
+        fs::create_dir(dirs)?;
+    }
+
+    fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
+
+    println!("Initialized git directory.");
+
+    Ok(())
 }
