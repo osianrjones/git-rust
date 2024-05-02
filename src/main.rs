fc0c708cbd018eecd89de39832a9e849d1970b28
osianrjones <osianrj03@gmail.com>
2024-05-02 20:24:38 +0100

    Cat-File functionality

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 #[allow(unused_imports)]
 use std::env;
-use std::fmt::format;
+use std::{fmt::format, io::Read};
 #[allow(unused_imports)]
 use std::fs;
 use std::vec;
+
+use clap::Error;
+use flate2::bufread::ZlibDecoder;
 
 fn main() {
 
     // Create default .git directory
     let args: Vec<String> = env::args().collect();
     match args[1].as_str() {
+        //git init
         "init" => {
             let init_result = init();
             if init_result.is_err() {
                 println!("Unable to create git directory.")
+            }
+        }
+        //$ git cat-file -p <blob_sha>
+        "cat-file" => {
+            //Ensure -p flag selected
+            if args[2] == "-p" {
+                let blob: String = args[3].parse().unwrap();
+                let cat_file_result = cat_print(&blob.as_str());
+            } else {
+                println!("No -p flag selected.")
             }
         }
         _ => println!("Unknown command: {}", args[1])
@@ -27,7 +41,7 @@ fn main() {
 #[allow(dead_code)]
 #[allow(unused_variables)]
 fn init() -> Result<(), std::io::Error>{
-    //Create .git exists
+    //Create .git dir
     let git_root_creation = fs::create_dir(".git")?;
 
     let init_dirs = vec![".git/objects", ".git/refs", ".git/HEAD"];
@@ -40,4 +54,22 @@ fn init() -> Result<(), std::io::Error>{
     println!("Initialized git directory.");
 
     Ok(())
+}
+
+fn cat_print(blob: &str) ->Result<(), std::io::Error> {
+    //This is the path the blob should exist at
+    let path = format!(".git/objects/{}/{}", &blob[0..2], &blob[2..]);
+
+    //Read the blob using Zlib
+    let blob_data = fs::read(&path).unwrap();
+    let mut holdr = ZlibDecoder::new(&blob_data[..]);
+    let mut decompressed = Vec::new();
+    holdr.read_to_end(&mut decompressed).unwrap();
+
+    //Split the null byte
+     let parts: Vec<&[u8]> = decompressed.split(|&x| x == 0).collect();
+     let content = parts[1];
+     print!("{}", String::from_utf8(content.to_vec()).unwrap());
+
+     Ok(())
 }
