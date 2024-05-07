00d1088e2dc49b3940a232a2abd04153f2c71cb9
osianrjones <osianrj03@gmail.com>
2024-05-07 21:15:01 +0100

    Create blob object

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #[allow(unused_imports)]
 use std::env;
-use std::{fmt::format, io::Read};
+use std::{fmt::format, hash, io::{BufWriter, Read, Write}, path::PathBuf};
 #[allow(unused_imports)]
 use std::fs;
 use std::vec;
-
 use clap::Error;
-use flate2::bufread::ZlibDecoder;
+use flate2::{bufread::ZlibDecoder, write::ZlibEncoder, Compression};
+use sha1::Digest;
 
 fn main() {
 
@@ -28,6 +28,18 @@ fn main() {
                 let _ = cat_print(&blob.as_str());
             } else {
                 println!("No -p flag selected.")
+            }
+        }
+        //git hash-object -w /path/to/file
+        "hash-object" => {
+            if args[2] == "-w" {
+                if !(args[3].is_empty()) {
+                    hash_object(args[3])
+                } else {
+                    println!("No path to file entered.")
+                }
+            } else {
+                println!("No -w flag selected.")
             }
         }
         _ => println!("Unknown command: {}", args[1])
@@ -56,7 +68,7 @@ fn init() -> Result<(), std::io::Error>{
     Ok(())
 }
 
-fn cat_print(blob: &str) ->Result<(), std::io::Error> {
+fn cat_print(blob: &str) -> Result<(), std::io::Error> {
     //This is the path the blob should exist at
     let path = format!(".git/objects/{}/{}", &blob[0..2], &blob[2..]);
 
@@ -72,4 +84,47 @@ fn cat_print(blob: &str) ->Result<(), std::io::Error> {
      print!("{}", String::from_utf8(content.to_vec()).unwrap());
 
      Ok(())
+}
+
+fn hash_object(path: String) -> Result<(), std::io::Error> {
+    //Open the path to the file
+    let mut fileData = fs::read(&path)?;
+    let mut buffer: Vec<u8> = Vec::new();
+
+    //Create the blob for the hash
+    let blob = format!("blob {}\0", fileData.len());
+
+    //Create the hash
+    let hash = {
+        let mut hasher = sha1::Sha1::new();
+        hasher.update(blob.as_bytes());
+        hasher.update(fileData);
+        hasher.finalize()
+    };
+
+    //Open or create .git path
+    let mut path = PathBuf::from(".git/objects/");
+    path.push(hex::encode(&hash[..1]));
+    if !path.is_dir() {
+        fs::create_dir(&path)?;
+    }
+
+    //Write the hash and encode it
+    path.push(hex::encode(&hash[1..]));
+
+    let output_file = fs::OpenOptions::new()
+    .write(true)
+    .create(true)
+    .open(&path);
+
+    let mut zlib_encoder =
+                ZlibEncoder::new(Vec::new(), Compression::default());
+    
+    zlib_encoder.write_all(blob.as_bytes());
+
+    zlib_encoder.write_all(&fileData);
+
+    println!("{}", hex::encode(hash));
+
+    Ok(())
 }
